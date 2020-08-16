b16aed7b1bb92db2e10626a939cb9520259c6a32
Alberto Cruz <albertocruzluis9@gmail.com>
2020-08-16 21:39:24 +0100

    Updates

---
+++ precache-manifest.5421a3e880a203369477bd0270a79d6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02d2941dc603275f08b1a5966191ddaf",
+    "url": "/TenerifeBeachApp/index.html"
+  },
+  {
+    "revision": "83adc0bcfbb321d09f3e",
+    "url": "/TenerifeBeachApp/static/css/main.acc487cb.chunk.css"
+  },
+  {
+    "revision": "6092e7f86629ed915841",
+    "url": "/TenerifeBeachApp/static/js/2.dcc17892.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/TenerifeBeachApp/static/js/2.dcc17892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83adc0bcfbb321d09f3e",
+    "url": "/TenerifeBeachApp/static/js/main.9c281a8a.chunk.js"
+  },
+  {
+    "revision": "26d0231a705f75851423",
+    "url": "/TenerifeBeachApp/static/js/runtime-main.a3ffe329.js"
+  }
+]);
